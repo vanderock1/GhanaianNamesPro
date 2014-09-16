75c186bfdd2033d5a55e3c78aaaa0ffb730ee4e9
vanderock1 <vanderock1@gmail.com>
2014-09-16 22:24:28 +0000

    added foundation files

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,17 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', function($scope) {
+	$scope.days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+})
 
-}]);
+.controller('PhoneListCtrl', function ($scope) {
+  $scope.phones = [
+    {'name': 'Nexus S',
+     'snippet': 'Fast just got faster with Nexus S.'},
+    {'name': 'Motorola XOOM™ with Wi-Fi',
+     'snippet': 'The Next, Next Generation tablet.'},
+    {'name': 'MOTOROLA XOOM™',
+     'snippet': 'The Next, Next Generation tablet.'}
+  ];
+});
