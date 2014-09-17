116633b31f4640313d66f100e13f3712ddfe59ff
vanderock1 <vanderock1@gmail.com>
2014-09-17 09:48:06 +0000

    added controller and service for login

--- app/app.js
+++ app/app.js
@@ -2,12 +2,82 @@
 
 // Declare app level module which depends on views, and components
 
-angular.module('myApp', [
+
+var parentPage = angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
   'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+]);
+
+
+parentPage.service('LoginService', function() {
+
+    this.status = function(){
+        FB.getLoginStatus(function(response) {
+            console.log(response);
+            if (response.status === 'connected') {
+                $scope.Loginstate = 'Logged in';
+                console.log('Logged in.');
+                console.log(response.authResponse);
+            }else{
+                $scope.Loginstate = 'Logged out';
+            }
+        });
+    };
+    this.login = function(){
+        FB.getLoginStatus(function(response) {
+            console.log(response);
+            if (response.status === 'connected') {
+                FB.api('/me',  function(response) {
+                    console.log(response);
+
+                });
+                $scope.Loginstate = 'Hello, Logged in';
+                console.log('Logged in.');
+                console.log(response.authResponse);
+            }
+            else {
+                FB.login(function(response){
+                    console.log(response);
+                    console.log(response.authResponse);
+                    FB.api('/me', {fields: 'gender'}, function(response) {
+                        console.log(response);
+
+                    });
+                }, {scope: 'public_profile,email',return_scopes: true});
+            }
+        });
+    };
+
+    this.logout = function (){
+        FB.logout(function(response) {
+            $scope.Loginstate = 'Logged out';
+        });
+    }
+});
+
+
+parentPage.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.otherwise({redirectTo: '/view1'});
+    //when view1 load facebook
 }]);
+
+parentPage.controller('fbLoginController',function($scope){
+
+    $scope.Loginstate = 'Logged out';
+    $scope.logMeth = 'loginfb()';
+    $scope.LogAction = 'Login';
+
+    $scope.loginfb = function(){
+        LoginService.login();
+    };
+    $scope.logoutfb = function(){
+        LoginService.logout();
+    };
+
+});
+
+$(document).foundation();
+
+
